f79aabe37a39fda8cf00123b1e28d3aac11be3a1
Spitelon <93149642+Spitelon@users.noreply.github.com>
2021-11-23 23:24:58 +0100

    List

---
+++ SubtitleTimeShifter/List.h
@@ -0,0 +1,66 @@
+#pragma once
+
+
+template<typename T>
+class List {
+
+	int count;
+	struct element {
+		T data;
+		element* next;
+		element* prev;
+		element(T _data) {
+			this->data = _data;
+			this->next = nullptr;
+			this->prev = nullptr;
+		}
+	};
+	element* first;
+	element* last;
+
+public:
+	List() {
+		this->count = 0;
+		this->first = nullptr;
+		this->last = nullptr;
+	}
+
+
+	void append(T _data) {
+
+		element* tmp = new element(_data);
+		if (count == 0) {
+			first = tmp;
+			last = tmp;
+		}
+		else {
+			last->next = tmp;
+			tmp->prev = last;
+			last = tmp;
+		}
+		count++;
+	}
+
+	T pop() {
+		T datapop;
+		if (count != 0) {
+			datapop = last->data;
+			element* tmp = last;
+			last = last->prev;
+			if (last != nullptr) {
+				last->next = nullptr;
+			}
+			delete tmp;
+			count--;
+		}
+		else datapop = -1;
+		return datapop;
+
+	}
+   
+	int getCount() {
+		return this->count;
+	}
+
+
+};
--- SubtitleTimeShifter/Main.cpp
+++ SubtitleTimeShifter/Main.cpp
@@ -1,12 +1,26 @@
-#include "SRT.h"
-#include "SRTComponent.h"
-#include "Time.h"
 #include <iostream>
+#include "List.h"
 
 using namespace std;
 
 int main() {
 
+	//List<int>Lista();
+	List<int>* Test = new List<int>();
+	Test->append(7);
+	Test->append(13);
+	Test->append(21);
+
+	cout << Test->getCount() << endl;
+	cout << Test->pop() << endl;
+	cout << Test->pop() << endl;
+	cout << Test->getCount() << endl;
+	cout << Test->pop() << endl;
+	cout << Test->pop() << endl;
+	Test->append(22);
+	cout << Test->getCount() << endl;
+	cout << Test->pop() << endl;
+	cout << Test->getCount() << endl;
 
 
 	return 0;
